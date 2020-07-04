d646f7a259b91a1ad1965b2e53eb436f568229ec
Fleur Jarman <fleur.jarman@granicus.com>
2020-07-04 08:24:21 +0100

    removed js from html pages and resized map

--- javascript/map.js
+++ javascript/map.js
@@ -5,8 +5,9 @@ function initMap() {
             lat: 54.456116, lng: -3.069333
         }
     });
+  
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+  
     var locations = [
         // bassenthwaite
         { lat: 54.683802, lng: -3.2271 },
@@ -19,6 +20,8 @@ function initMap() {
             label: labels[i % labels.length]
         });
     });
+  
     var markerCluster = new MarkerClusterer(map, markers,
         { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-}
+  }
+  
--- javascript/photogallery.js
+++ javascript/photogallery.js
@@ -1,17 +1,18 @@
+var photoIndex = 0;
+showPhotos();
 function showPhotos() {
-    var i;
-    var photos = document.getElementsByClassName("lakeimgs");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) 
-    {
-      photos[i].style.display = "none";  
-    }
-    photoIndex++;
-    if (photoIndex > photos.length) {photoIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    photos[photoIndex-1].style.display = "block";  
-    dots[photoIndex-1].className += " active";
-    // setTimeout(showPhotos, 2000); // Change image every 2 seconds
+  var i;
+  var photos = document.getElementsByClassName("lakeimgs");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < photos.length; i++) {
+    photos[i].style.display = "none";
   }
+  photoIndex++;
+  if (photoIndex > photos.length) { photoIndex = 1 }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  photos[photoIndex - 1].style.display = "block";
+  dots[photoIndex - 1].className += " active";
+  setTimeout(showPhotos, 2000); // Change image every 2 seconds
+}
