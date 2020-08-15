e3e4246b7f76621107dbfa08137ca0905ca65e0b
Fleur Jarman <fleur.jarman@granicus.com>
2020-08-15 21:13:00 +0100

    added place holder items for all categories

--- javascript/map.js
+++ javascript/map.js
@@ -1,21 +1,40 @@
 
+const locationscat1 = [
+  //  Armathwaite Hall Hotel & Spa
+  { lat: 54.6816834, lng: -3.2364325 }
+ 
+];
 
-// let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const locationscat2 = [
+  //  The Drunken Duck Inn & Restaurant
+  { lat: 54.4065763, lng: -3.0086745 }
+ 
+];
 
 
+const locationscat3 = [
+  //  Great Langdale Campsite
+  { lat: 54.4430, lng: -3.0987 },
+  // Hill of Oaks Caravan & Lodge Park
+  { lat: 54.300411, lng: -2.947160 },
+  //Low Wray Campsite
+  {lat: 54.400810, lng: -2.966060 }
+];
 
 
-const locations = [
-  //  windermere
-  { lat: 54.380204, lng: -2.934750 },
+const locationscat4 = [
+  //  Helvellyn
+  { lat: 54.5268, lng: -3.0172 },
   // bassenthwaite
-  { lat: 54.683802, lng: -3.2271 }
+  { lat: 54.683802, lng: -3.2271 },
+  //scafell pike
+  {lat: 54.4542, lng: -3.2116 }
 ];
 
 
 let markers = [];
 let map;
-
+// let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // The below loads the map to the adventure page
 function initMap() {
@@ -26,20 +45,46 @@ function initMap() {
 }
 
 
+function showListcat1() {
+  clearMarkers();
+
+  for (let i = 0; i < locationscat1.length; i++) {
+    addMarkerWithTimeout(locationscat1[i], i * 200);
+  }
+}
+
+
+function showListcat2() {
+  clearMarkers();
+
+  for (let i = 0; i < locationscat2.length; i++) {
+    addMarkerWithTimeout(locationscat2[i], i * 200);
+  }
+}
+
+function showListcat3() {
+  clearMarkers();
+
+  for (let i = 0; i < locationscat3.length; i++) {
+    addMarkerWithTimeout(locationscat3[i], i * 200);
+  }
+}
 
 function showListcat4() {
   clearMarkers();
 
-  for (let i = 0; i < locations.length; i++) {
-    addMarkerWithTimeout(locations[i], i * 200);
+  for (let i = 0; i < locationscat4.length; i++) {
+    addMarkerWithTimeout(locationscat4[i], i * 200);
   }
 }
+
+
 
 function addMarkerWithTimeout(position, timeout) {
   window.setTimeout(() => {
     markers.push(
       new google.maps.Marker({
-        position: { lat: 54.456116, lng: -3.069333 },
+        position: position,
         map: map,
         animation: google.maps.Animation.DROP
       })
