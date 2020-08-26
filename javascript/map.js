f4a14cbdabd7a85402e42090d33a229f2e2f36a4
Fleur Jarman <fleur.jarman@granicus.com>
2020-08-26 12:47:15 +0100

    add destinations and removed burger button

--- javascript/map.js
+++ javascript/map.js
@@ -1,23 +1,35 @@
 
 const locationscat1 = [
   //  Armathwaite Hall Hotel & Spa
-  { lat: 54.6816834, lng: -3.2364325 }
+  { title: "Armathwaite Hall Hotel & Spa - CA12 4RE", lat: 54.6816834, lng: -3.2364325,description: "Set in a revamped, 11th-century estate bordering Bassenthwaite Lake, this upscale resort is 8 miles from Cockermouth....", image: "<a href='https://www.armathwaite-hall.com/' target='_blank'><img src='images/ARMATHWAITE_hall.jpg'></a>"  },
+  { title: "Lakes Lodge - LA23 1AF", lat: 54.3803, lng: -2.9058,description: "Off the A591 at the heart of the Lake District, this quaint, family-run B&B set in a converted 1860s bank is a minute's walk from Windermere...", image: "<a href='http://www.lakes-lodge.co.uk/' target='_blank'><img src='images/Lakeslodge.jpg'></a>"  },
+  { title: "Windermere Manor Hotel - LA23 1JF", lat: 54.3836, lng: -2.9177,description: "Set in an 1850s stone manor house, this cozy hotel is a mile from Lake Windermere. It's 7.2 miles from Wray Castle and 9 miles from the Beatrix Potter Museum...", image: "<a href='https://www.windermeremanorhotel.com/' target='_blank'><img src='images/windermere-manor.jpg'></a>"  },
+  { title: "Gilpin Hotel & Lake House - LA23 3NE", lat: 54.3548, lng: -2.9090, description: "Set among landscaped gardens in a 1901 Georgian-style country house with an annexe, this upscale hotel is 2.6 miles from...", image: "<a href='https://thegilpin.co.uk/' target='_blank'><img src='images/Gilpin.jpg'></a>"  },
+  { title: "Armathwaite Hall Hotel & Spa - CA12 4RE", lat: 54.6816834, lng: -3.2364325,description: "Set in a revamped, 11th-century estate bordering Bassenthwaite Lake, this upscale resort is 8 miles from Cockermouth....", image: "<a href='https://www.armathwaite-hall.com/' target='_blank'><img src='images/ARMATHWAITE_hall.jpg'></a>"  },
+  { title: "Armathwaite Hall Hotel & Spa - CA12 4RE", lat: 54.6816834, lng: -3.2364325,description: "Set in a revamped, 11th-century estate bordering Bassenthwaite Lake, this upscale resort is 8 miles from Cockermouth....", image: "<a href='https://www.armathwaite-hall.com/' target='_blank'><img src='images/ARMATHWAITE_hall.jpg'></a>"  },
+  { title: "Armathwaite Hall Hotel & Spa - CA12 4RE", lat: 54.6816834, lng: -3.2364325,description: "Set in a revamped, 11th-century estate bordering Bassenthwaite Lake, this upscale resort is 8 miles from Cockermouth....", image: "<a href='https://www.armathwaite-hall.com/' target='_blank'><img src='images/ARMATHWAITE_hall.jpg'></a>"  },
+  { title: "Armathwaite Hall Hotel & Spa - CA12 4RE", lat: 54.6816834, lng: -3.2364325,description: "Set in a revamped, 11th-century estate bordering Bassenthwaite Lake, this upscale resort is 8 miles from Cockermouth....", image: "<a href='https://www.armathwaite-hall.com/' target='_blank'><img src='images/ARMATHWAITE_hall.jpg'></a>"  }
 
 ];
 
 const locationscat2 = [
   //  The Drunken Duck Inn & Restaurant
-  { lat: 54.4065763, lng: -3.0086745 }
+  { title: "The Drunken Duck Inn & Restaurant - LA22 0NG", lat: 54.4065763, lng: -3.0086745,description: "Set among the rolling countryside of Lake District National Park, this upscale inn is 3 miles from the A591 road, and 4 miles from the Langdale Hotel and Spa...", image: "<a href='https://drunkenduckinn.co.uk/' target='_blank'><img src='images/drunken-duck.jpg'></a>"},
+  { title: "The Drunken Duck Inn & Restaurant - LA22 0NG", lat: 54.4065763, lng: -3.0086745,description: "Set among the rolling countryside of Lake District National Park, this upscale inn is 3 miles from the A591 road, and 4 miles from the Langdale Hotel and Spa...", image: "<a href='https://drunkenduckinn.co.uk/' target='_blank'><img src='images/drunken-duck.jpg'></a>"},
+  { title: "The Drunken Duck Inn & Restaurant - LA22 0NG", lat: 54.4065763, lng: -3.0086745,description: "Set among the rolling countryside of Lake District National Park, this upscale inn is 3 miles from the A591 road, and 4 miles from the Langdale Hotel and Spa...", image: "<a href='https://drunkenduckinn.co.uk/' target='_blank'><img src='images/drunken-duck.jpg'></a>"},
+  { title: "The Drunken Duck Inn & Restaurant - LA22 0NG", lat: 54.4065763, lng: -3.0086745,description: "Set among the rolling countryside of Lake District National Park, this upscale inn is 3 miles from the A591 road, and 4 miles from the Langdale Hotel and Spa...", image: "<a href='https://drunkenduckinn.co.uk/' target='_blank'><img src='images/drunken-duck.jpg'></a>"},
+  { title: "The Drunken Duck Inn & Restaurant - LA22 0NG", lat: 54.4065763, lng: -3.0086745,description: "Set among the rolling countryside of Lake District National Park, this upscale inn is 3 miles from the A591 road, and 4 miles from the Langdale Hotel and Spa...", image: "<a href='https://drunkenduckinn.co.uk/' target='_blank'><img src='images/drunken-duck.jpg'></a>"},
+  { title: "The Drunken Duck Inn & Restaurant - LA22 0NG", lat: 54.4065763, lng: -3.0086745,description: "Set among the rolling countryside of Lake District National Park, this upscale inn is 3 miles from the A591 road, and 4 miles from the Langdale Hotel and Spa...", image: "<a href='https://drunkenduckinn.co.uk/' target='_blank'><img src='images/drunken-duck.jpg'></a>"},
+  { title: "The Drunken Duck Inn & Restaurant - LA22 0NG", lat: 54.4065763, lng: -3.0086745,description: "Set among the rolling countryside of Lake District National Park, this upscale inn is 3 miles from the A591 road, and 4 miles from the Langdale Hotel and Spa...", image: "<a href='https://drunkenduckinn.co.uk/' target='_blank'><img src='images/drunken-duck.jpg'></a>"},
+  { title: "The Drunken Duck Inn & Restaurant - LA22 0NG", lat: 54.4065763, lng: -3.0086745,description: "Set among the rolling countryside of Lake District National Park, this upscale inn is 3 miles from the A591 road, and 4 miles from the Langdale Hotel and Spa...", image: "<a href='https://drunkenduckinn.co.uk/' target='_blank'><img src='images/drunken-duck.jpg'></a>"},
 
 ];
 
 
 const locationscat3 = [
-  //  Great Langdale Campsite
+
   { title: "Great Langdale Campsite - LA22 9JU", lat: 54.4430, lng: -3.0987, description: "A relaxed, natural site set in the heart of the Great Langdale Valley. With the towering Langdale Pikes looming above it, Great Langdale Campsite is a particularly dramatic spot to pitch up for...", image: "<a href='https://www.nationaltrust.org.uk/holidays/great-langdale-campsite-lake-district' target='_blank'><img src='images/GLCampsite.jpg'></a>" },
-  // Hill of Oaks Caravan & Lodge Park
   { title: "Hill of Oaks Caravan & Lodge Park - LA12 8NR", lat: 54.300411, lng: -2.947160, description: "Hill of Oaks is a dog friendly park on the shores of Lake Windermere welcoming touring caravans, motor homes and...", image: "<a href='https://www.hillofoaks.co.uk/' target='_blank'><img src='images/HOO.jpg'></a>" },
-  //Low Wray Campsite
   { title: "Low Wray Campsite", lat: 54.400810, lng: -2.966060, description: "A great place to camp in Low Wray!" }
 ];
 
